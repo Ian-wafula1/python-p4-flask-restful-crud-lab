f302b5208f2b98ea1f898e231279fc9fc9f6f119
Ian Wafula <neocorpse@proton.me>
2025-06-16 18:58:39 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -46,6 +46,22 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in (json := request.get_json()):
+            setattr(plant, attr, json[attr])
+            
+        db.session.add(plant)
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('no content', 204)
+            
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
